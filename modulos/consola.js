a53f2387175173a4320e69376332a07523953d31
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-20 11:06:56 -0500

    como aprovechar bien la consola

---
+++ modulos/consola.js
@@ -0,0 +1,66 @@
+// console.log('algo');
+
+// console.info('algo');
+
+// console.error('algo');
+
+// console.warn('algo');
+
+
+// var tabla=[
+//     {
+//         a:1,
+//         b: 'z'
+//     },
+//     {
+//         a:2,
+//         b:'x'
+//     }
+// ]
+//  console.table(tabla)
+
+
+
+// console.group('conversacion') 
+// console.log('hola');
+// console.group('blabla')
+// console.log('bla bla bla');
+// console.log('bla bla bla');
+// console.log('bla bla bla');
+// console.groupEnd('bla');
+// console.log('bla bla bla');
+// console.log('adios');
+// console.groupEnd('conver')
+
+// console.log('otra cosa muy xd... xd');
+
+
+
+
+// function function1(){
+//     console.group('function 1');
+//     console.log('esto es de la funcion 1');
+//     console.log('esto tambien');
+//     console.log('esto igual :D');
+//     function2();
+//     console.log('he vuelto a la 1')
+//     console.groupEnd('funcition 1');
+// }
+
+
+// function function2(){
+//     console.group('function 2')
+//     console.log('ahora estanos en la funcion 2')
+//     console.groupEnd('function 2')
+// }
+
+// console.log('empezamos')
+// function1();
+
+
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
---
+++ modulos/fs.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function leer(ruta, cb){
+    fs.readFile(ruta, (err, data)=>{
+        cb(data.toString());
+    })
+}
+
+function escribir(ruta, contenido, cb){
+    fs.writeFile(ruta, contenido, function(err){
+        if(err){
+            console.error('no he podido escribir', err)
+        }else{
+            console.log('se ha escrito correctamente')
+        }
+    });
+}
+
+function borrar(ruta, cb){
+    fs.unlink(ruta, cb);
+}
+
+// escribir(__dirname + '/archivo1.txt', 'soy un archivo nuevo hecho a base de un codigo de fs y con el modulo de writeFile :D', console.log);
+// leer(__dirname + '/archivo.txt', console.log)
+borrar(__dirname + '/archivo1.txt', console.log);
