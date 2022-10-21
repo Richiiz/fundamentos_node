dc8bc664828a2431eada86d91f3de4709a90ce17
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-21 15:24:57 -0500

    se aprende a hacer una prueba de rendimiento o llamada en ingles Benchmark usando console.time y console.timeEnd

---
+++ trucos/benchmarking.js
@@ -0,0 +1,38 @@
+// para saber cuanto tarda en ejecutarse nuestro codigo o una parte de este usamos console.time y console.timeEnd
+
+console.time('todo');
+
+let suma = 0;
+
+console.time('bucle');
+for (let i = 0; i<1000000000; i++){
+    suma += 1;
+}
+console.timeEnd('bucle');
+
+
+let suma2 = 0;
+
+console.time('bucle2');
+for (let j = 0; j<100000000; j++){
+    suma2 += 1;
+}
+console.timeEnd('bucle2');
+
+console.time('asincrono');
+console.log('comienza el proceso asincrono')
+asincrono()
+.then(()=>{
+    console.timeEnd('asincrono');
+})
+
+console.timeEnd('todo')
+
+function asincrono(){
+    return new Promise((resolve)=>{
+        setTimeout(function(){
+            console.log('termina el proceso asincrono')
+            resolve();
+        })
+    })
+}
