2f5ed6c3d3f6048e38654f8af5a2aa917ec0ad3c
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-21 12:04:38 -0500

    bla bla

--- paquetes/npm/index.js
+++ paquetes/npm/index.js
@@ -1,2 +1,4 @@
 const isOdd  = require ('is-odd');
 console.log(isOdd(2));
+
+// pagina pa buscar paquetes: https://www.npmjs.com/
---
+++ paquetes/npm/modulo/index.js
@@ -0,0 +1,5 @@
+//traer modulo
+const modulo = require('./modulo');
+//ejecutar una funcion del modulo
+
+console.log(modulo)
